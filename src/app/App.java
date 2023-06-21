8987c443e3f77ca059355577a5e4160ee98ec10f
Nicholas <nicholas.s.luz@gmail.com>
2023-06-21 13:21:53 -0300

    remover leituras terminal do app

--- src/app/App.java
+++ src/app/App.java
@@ -7,10 +7,7 @@ import java.util.stream.Collectors;
 import src.com.acmehandel.dados.*;
 import src.com.acmehandel.modelo.*;
 import src.com.acmehandel.util.CSVReader;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonArray;
+import com.google.gson.*;
 
 public class App {
   private Frota frota = new Frota();
@@ -19,7 +16,6 @@ public class App {
   private Portos portos = new Portos();
   private Distancias distancias = new Distancias();
   private TiposCargas tiposCargas = new TiposCargas();
-  private Scanner teclado = new Scanner(System.in);
   private PrintStream standard = System.out;
   private PrintStream streamSaida;
 
@@ -47,88 +43,9 @@ public class App {
     return tiposCargas;
   }
 
-  public int escolherOpcaoMenuAdicionarDado() {
-    boolean continuar = false;
-    int resposta = 0;
-    while (!continuar) {
-      try {
-        resposta = Integer.parseInt(teclado.nextLine());
-
-        if (resposta < 0 || resposta > 6) {
-          throw new InputMismatchException();
-        }
-        continuar = true;
-      } catch (InputMismatchException | NumberFormatException e) {
-        System.out.println("Valor inválido, insira valor da lista.");
-      }
-    }
-    return resposta;
-  }
-
-  public void tratarRespostaMenuAdicionarDado(int resposta) {
-    switch (resposta) {
-      case 1:
-        cadastraNovoPorto();
-        break;
-      case 2:
-        cadastraNovoNavio();
-        break;
-      case 3:
-        cadastraNovoCliente();
-        break;
-      case 4:
-        cadastraNovoTipoCarga();
-        break;
-      case 5:
-        cadastraNovaCarga();
-        break;
-      case 6:
-        break;
-      case 0:
-        System.out.println("Encerrando o programa");
-        System.exit(0);
-        break;
-      default:
-        System.out.println("Erro inesperado aconteceu. Encerrando o programa.");
-        System.exit(0);
-    }
-  }
-
   public void readFiles(String filePrefix) {
     CSVReader filesRead = new CSVReader();
     filesRead.readFiles(filePrefix, portos, distancias, frota, clientes, tiposCargas, cargas);
-  }
-
-  public void cadastraNovoPorto() {
-    int sair = 1;
-    try {
-      do {
-        System.out.println("Digite o id do porto (id tem que ser unico):");
-        int id = teclado.nextInt();
-        if (portos.checkPortoIdJaExiste(id)) {
-          throw new IllegalArgumentException("id do porto ja cadastrado");
-        }
-        System.out.println("Digite o nome do porto:");
-        String nome = teclado.nextLine();
-        teclado.nextLine();
-        System.out.println("Digite o país do porto:");
-        String pais = teclado.nextLine();
-        teclado.nextLine();
-
-        Porto p = new Porto(id, nome, pais);
-        if (!portos.adicionaPorto(p)) {
-          throw new IllegalArgumentException("Ocorreu um erro: id do porto ja cadastrado");
-        }
-        adicionaDistanciaPortos(p);
-        portos.sort();
-        System.out.println("Para sair, digite \"0\": ");
-        sair = teclado.nextInt();
-      } while (sair != 0);
-    } catch (InputMismatchException e) {
-      System.out.println("Input inválido, insira número inteiro");
-    } catch (IllegalArgumentException e) {
-      e.getMessage();
-    }
   }
 
   public void adicionaDistanciaPortos(Porto p) {
@@ -148,170 +65,6 @@ public class App {
           }
         }
       }
-    }
-  }
-
-  public void cadastraNovoNavio() {
-    int sair = 1;
-    try {
-      do {
-        System.out.println("Digite o nome do navio (nomes tem que ser unicos):");
-        String nome = teclado.nextLine();
-        teclado.nextLine();
-        if (frota.checkNomeNavioJaExiste(nome)) {
-          throw new IllegalArgumentException("Nome do navio ja cadastrado");
-        }
-        System.out.println("Digite a velocidade do navio:");
-        double velocidade = teclado.nextDouble();
-        System.out.println("Digite a autonomia do navio:");
-        double autonomia = teclado.nextDouble();
-        System.out.println("Digite o custo por milha basico do navio:");
-        double custoPorMilha = teclado.nextDouble();
-
-        Navio n = new Navio(nome, velocidade, autonomia, custoPorMilha);
-        if (!frota.adicionaNavio(n)) {
-          System.out.println("Ocorreu um erro: Ja existe navio com este nome");
-        }
-
-        frota.sort();
-        System.out.println("Para sair, digite \"0\": ");
-        sair = teclado.nextInt();
-      } while (sair != 0);
-    } catch (IllegalStateException e) {
-      e.getMessage();
-    }
-  }
-
-  public void cadastraNovoTipoCarga() {
-    int sair = 1;
-    try {
-      do {
-        System.out.println("Digite o codigo do tipo de carga (codigo tem que ser unico):");
-        int codigo = teclado.nextInt();
-        if (tiposCargas.checkTipoCargaJaExiste(codigo)) {
-          throw new IllegalArgumentException("Codigo do tipo de carga ja cadastrado");
-        }
-        System.out.println("Digite a descricao do tipo de carga:");
-        String descricao = teclado.nextLine();
-        teclado.nextLine();
-        System.out.println("Digite a categoria (DURAVEL ou PERECIVEL):");
-        String categoria = teclado.nextLine();
-        teclado.nextLine();
-        try {
-          CategoriaCarga.valueOf(categoria);
-        } catch (IllegalArgumentException e) {
-          throw new IllegalArgumentException("Categoria deve ser DURAVEL ou PERECIVEL!!");
-        }
-
-        switch (CategoriaCarga.valueOf(categoria)) {
-          case DURAVEL:
-            System.out.println("Digite o setor:");
-            String setor = teclado.nextLine();
-            teclado.nextLine();
-            System.out.println("Digite o material principal:");
-            String materialPrincipal = teclado.nextLine();
-            teclado.nextLine();
-            System.out.println("Digite o percentual do IPI:");
-            double percentualIPI = teclado.nextDouble();
-            CargaDuravel cargaD = new CargaDuravel(codigo, descricao, setor, materialPrincipal, percentualIPI);
-            tiposCargas.adicionaTipoCarga(cargaD);
-            break;
-          case PERECIVEL:
-            System.out.println("Digite a origem:");
-            String origem = teclado.nextLine();
-            teclado.nextLine();
-            System.out.println("Digite o tempo maximo de validade:");
-            int validade = teclado.nextInt();
-            CargaPerecivel cargaP = new CargaPerecivel(codigo, descricao, origem, validade);
-            tiposCargas.adicionaTipoCarga(cargaP);
-            break;
-        }
-        tiposCargas.sort();
-        System.out.println("Para sair, digite \"0\": ");
-        sair = teclado.nextInt();
-      } while (sair != 0);
-    } catch (IllegalArgumentException i) {
-      i.getMessage();
-    }
-  }
-
-  public void cadastraNovaCarga() {
-    int sair = 1;
-    try {
-      do {
-        System.out.println("Digite o codigo da carga (codigo tem que ser unico):");
-        int codigo = teclado.nextInt();
-        if (cargas.checkCargaIdJaExiste(codigo)) {
-          throw new IllegalArgumentException("codigo ja cadastrado");
-        }
-        System.out.println("Digite o id do cliente:");
-        int id = teclado.nextInt();
-        System.out.println("Digite o id do porto de origem:");
-        int idOrigem = teclado.nextInt();
-        System.out.println("Digite o id do porto de destino:");
-        int idDestino = teclado.nextInt();
-        System.out.println("Digite o peso:");
-        int peso = teclado.nextInt();
-        System.out.println("Digite o valor declarado:");
-        double valorDeclarado = teclado.nextDouble();
-        System.out.println("Digite o tempo maximo:");
-        int tempoMaximo = teclado.nextInt();
-        System.out.println("Digite o id do tipo de carga:");
-        int idTipoCarga = teclado.nextInt();
-        System.out.println("Digite a prioridade da entrega (RAPIDO ou BARATO):");
-        String prioridade = teclado.nextLine();
-        try {
-          Prioridade.valueOf(prioridade);
-        } catch (IllegalArgumentException e) {
-          throw new IllegalArgumentException("Prioridade deve ser RAPIDO ou BARATO!!");
-        }
-        teclado.nextLine();
-
-        Carga carga = new Carga(codigo, id, idOrigem, idDestino, peso, valorDeclarado, tempoMaximo, idTipoCarga,
-            prioridade, "PENDENTE");
-
-        cargas.adicionaCarga(carga);
-        cargas.sort();
-
-        System.out.println("Para sair, digite \"0\": ");
-        sair = teclado.nextInt();
-      } while (sair != 0);
-    } catch (IllegalStateException e) {
-      e.getMessage();
-    }
-  }
-
-  public void cadastraNovoCliente() {
-    int sair = 1;
-    try {
-      do {
-        System.out.println("Digite o codigo do cliente (codigo tem que ser unico):");
-        int codigo = teclado.nextInt();
-        if (clientes.checkCodClienteJaExiste(codigo)) {
-          throw new IllegalArgumentException("codigo ja cadastrado");
-        }
-        System.out.println("Digite o nome do cliente:");
-        String nome = teclado.nextLine();
-        teclado.nextLine();
-        System.out.println("Digite o email do cliente:");
-        String email = teclado.nextLine();
-        teclado.nextLine();
-        if (clientes.checkEmailClienteJaExiste(email)) {
-          throw new IllegalArgumentException("email ja cadastrado");
-        }
-
-        Cliente c = new Cliente(codigo, nome, email);
-        if (!clientes.adicionaCliente(c)) {
-          System.out.println("Ocorreu um erro: codigo ou email ja cadastrados.");
-        }
-
-        clientes.sort();
-
-        System.out.println("Para sair, digite \"0\": ");
-        sair = teclado.nextInt();
-      } while (sair != 0);
-    } catch (IllegalStateException e) {
-      e.getMessage();
     }
   }
 
@@ -487,14 +240,13 @@ public class App {
   }
 
   private static JsonArray createJsonArray(List<?> list) {
-        JsonArray jsonArray = new JsonArray();
-        for (Object item : list) {
-            Gson gson = new Gson();
-            JsonObject itemObject = gson.toJsonTree(item).getAsJsonObject();
-            jsonArray.add(itemObject);
-        }
-        return jsonArray;
+    JsonArray jsonArray = new JsonArray();
+    for (Object item : list) {
+      Gson gson = new Gson();
+      JsonObject itemObject = gson.toJsonTree(item).getAsJsonObject();
+      jsonArray.add(itemObject);
     }
-
+    return jsonArray;
+  }
 }
 
